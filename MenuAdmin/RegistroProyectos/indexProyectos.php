d816d4036e32112ffa2bfff69c9cd6d45f8632ca
Erik <beltraberik@gmail.com>
2023-10-29 16:41:37 -0700

    Se agrego la opcion de ver los estados del proyecto en un select/option

--- MenuAdmin/RegistroProyectos/indexProyectos.php
+++ MenuAdmin/RegistroProyectos/indexProyectos.php
@@ -5,23 +5,24 @@
 session_start();
 
 if (!isset($_SESSION['admin_name'])) {
-   header('location:../../Alertas/warning.html');
+    header('location:../../Alertas/warning.html');
 }
 ?>
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Gestión de Proyectos</title>
-        <link rel="stylesheet" href="">
-    </head>
 
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <title>Gestión de Proyectos</title>
+    <link rel="stylesheet" href="">
+</head>
 
-        <div class = "hero">
-            <a href="../index.php">Regresar al menú</a>
-            <h1 class="colortexto">Registro de Proyectos</h1>
-            <form method="post" action="addProyecto.php" class="colortexto">
+<body>
+
+    <div class="hero">
+        <a href="../index.php">Regresar al menú</a>
+        <h1 class="colortexto">Registro de Proyectos</h1>
+        <form method="post" action="addProyecto.php" class="colortexto">
             <label>Nombre del proyecto: * <input type="text" name="txtNombre"></label>
             <br>
             <label>Descripción del proyecto: * <input type="text" name="txtDes"></label>
@@ -32,7 +33,32 @@ if (!isset($_SESSION['admin_name'])) {
             <br>
             <label>Fecha de finalización: *<input type="date" name="F-fin"></label>
             <br>
-            <label>Estado del proyecto: *<input type="text" name="estado"></label>
+            <label>Estado del proyecto: *
+                <select name="estado">
+                    <?php
+                    include '../../conexion.php';
+                    $conexion = new conexion();
+                    if ($conexion->connect()) {
+                        $con = $conexion->getConexion();
+
+                        $query = "SELECT * FROM Estado";
+                        $resultado = $conexion->exeqSelect($query);
+                        var_dump($resultado);
+
+                        if ($resultado) {
+                            while ($row = mysqli_fetch_array($resultado)) {
+                                echo "<option value='" . $row['Codigo'] . "'>" . $row['nombre'] . "</option>";
+                            }
+                        } else {
+                            echo "Error en la consulta: " . mysqli_error($con);
+                        }
+                    } else {
+                        echo "Error en la conexión: " . mysqli_error($con);
+                    }
+                    ?>
+                </select>
+
+            </label>
             <br>
 
             <input type="reset" value="Cancel">
@@ -42,6 +68,7 @@ if (!isset($_SESSION['admin_name'])) {
             <button>Roles y permisos</button>
         </nav>
     </div>
-        
-    </body>
+
+</body>
+
 </html>
