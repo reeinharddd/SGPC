3756c8fb0455e47fa8a210ffe79b17523617379d
Erik <beltraberik@gmail.com>
2023-11-01 21:13:07 -0700

    Se agregaron todos los proyectos al iniciar sesion el usuario

--- MenuUsuario/index.php
+++ MenuUsuario/index.php
@@ -18,7 +18,14 @@ if (!isset($_SESSION['user_name'])) {
     <link rel="icon" href="../img/Logo1.png" type = "image/png">
   </head>
   <body>
-    <?php
+  
+
+    <header class = "header">
+      
+      <div><img src="../img/Logo1.png" alt="" width="100px" height="100px"></div>
+    
+      <nav class = "navigation">
+      <?php
     if (isset($_SESSION['user_name'])) {
       echo "<h1>Bienvenido usuario:" . $_SESSION['user_name'] . "</h1>";
     }else{
@@ -26,12 +33,6 @@ if (!isset($_SESSION['user_name'])) {
     }
     
     ?>
-
-    <header class = "header">
-      <div><img src="../img/Logo1.png" alt="" width="100px" height="100px"></div>
-      <input type="checkbox" id = "toggle">
-      <label for="toggle"> <img src="../img/menu.svg" alt="menu"></label>
-      <nav class = "navigation">
         <ul>
             <li><a href="#">Proyectos</a>
               <ul>
@@ -45,6 +46,58 @@ if (!isset($_SESSION['user_name'])) {
         </ul>
       </nav>
     </header>
+    <section>
+      <div class="welcome-and-projects">
+      
+      </div>
+      <div class="project-list">
+      <?php
+      include "../conexion.php";
+
+      $conexion = new conexion();
+      if ($conexion->connect()){
+        $con = $conexion->getConexion();
+      $sql = "SELECT 
+Proyectos.numProyecto,
+      Proyectos.Nombre,
+      Proyectos.Descripcion,
+      Proyectos.Ubicacion,
+      Proyectos.fechaInicio,
+      Proyectos.fechaFinal,
+      Proyectos.Estado
+  FROM 
+      Proyectos
+  JOIN 
+      USUARIO_PROYECTO
+  ON 
+      Proyectos.numProyecto = USUARIO_PROYECTO.Proyecto
+  WHERE 
+      USUARIO_PROYECTO.Usuario = '".$_SESSION['id']."'";
+
+      $result = $conexion->exeqSelect($sql);
+
+        if ($result) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo "<div class='project-box'>";
+            echo "<div class='project-info'>";
+            echo "<h3>Nombre: " . $row["Nombre"] . "</h3>";
+            echo "<p>Descripción: " . $row["Descripcion"] . "</p>";
+            echo "<p>Ubicación: " . $row['Ubicacion'] . "</p>";
+            echo "<p>Fecha Inicio: " . $row['fechaInicio'] . "</p>";
+            echo "<p>Fecha Final: " . $row['fechaFinal'] . "</p>";
+            echo "<p>Estado: " . $row['Estado'] . "</p>";
+            echo "</div>";
+            echo "<a href='Proyectos.php?data-numProyecto=" . $row["numProyecto"] . "' class='details-button'>Ver detalles</a>";
+            echo "</div>";
+        }
+        echo "</div>";
+    } else {
+        echo "No se encontraron proyectos.";
+    }
+      }
+      ?>
+      </div>
+    </section>
     <div class = "hero"></div>
   </body>
 </html>
