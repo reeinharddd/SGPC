b2c49448cb35fd7afbd17e729a8706960d5dc029
Erik <beltraberik@gmail.com>
2023-11-01 21:11:56 -0700

    Se agregaron datos para ver toda la informacion del proyecto seleccionado

--- MenuUsuario/Proyectos.php
+++ MenuUsuario/Proyectos.php
@@ -5,21 +5,42 @@
 session_start();
 
 if (!isset($_SESSION['user_name'])) {
-   header('location:../Alertas/warning.html');
+    header('location:../Alertas/warning.html');
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyectos</title>
     <link rel="stylesheet" href="../css/main.css">
-    <link rel="icon" href="../img/Logo1.png" type = "image/png">
+    <link rel="icon" href="../img/Logo1.png" type="image/png">
 </head>
-<body class = "cuerpo">
+
+<body class="cuerpo">
     <a href="index.php">Regresar al menú</a>
     <br>
-    En esta parte iran todos los proyectos que esten vinculados al usuario
+    <?php
+    include "../conexion.php";
+    $conexion = new conexion();
+    if ($conexion->connect()){
+        $con = $conexion->getConexion();
+        $proyecto = $_GET['data-numProyecto'];
+
+        $query = "SELECT * from Proyectos where numProyecto = '$proyecto' ";
+        
+        $result = $conexion->exeqSelect($query);
+        if ($result){
+            while ($row = mysqli_fetch_assoc($result)){
+                echo "<h3>Nombre: " . $row["Nombre"] . "</h3>";
+            }
+            
+        }
+    }
+
+    ?>    
 </body>
+
 </html>
