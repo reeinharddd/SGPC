a13037ff471fc530b6e6eab6077e3679e9058af6
Erik <beltraberik@gmail.com>
2023-11-07 11:26:37 -0800

    Se modifico el index usuario y estilos

--- MenuUsuario/index.php
+++ MenuUsuario/index.php
@@ -3,12 +3,13 @@ session_start();
 
 if (!isset($_SESSION['user_name'])) {
     header('location:../Alertas/warning.html');
-    exit; // Terminar el script después de la redirección
+    exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,34 +17,33 @@ if (!isset($_SESSION['user_name'])) {
     <link rel="stylesheet" href="../css/main.css">
     <link rel="icon" href="../img/Logo1.png" type="image/png">
 </head>
+
 <body>
     <header class="header">
-        <div class="user-info">
-            <!-- Mostrar la imagen del usuario, su nombre y rango aquí -->
-            <img src="../img/account-icon-user-icon-vector-graphics_292645-552.avif" alt="Nombre del usuario">
-            <h3><?php echo $_SESSION['user_name']; ?> <p>Usuario</p></h3>
-           
-        </div>
         <div class="logo">
-            <!-- Logo de la empresa -->
-            <img  src="../img/Logo1.png" alt="Logo de la empresa">
+            <img src="../img/Logo1.png" alt="Logo de la empresa">
         </div>
-        <nav class="navigation">
-            <ul>
-                <li><a href="#">Proyectos</a></li>
-                <li><a href="Calendario/Calendario.php">Calendario</a></li>
-                <li><a href="Contacto.php">Contacto</a></li>
-                <li><a href="../InicioSesion/logout.php">Cerrar sesión</a></li>
-            </ul>
-        </nav>
+        <div class="user-info">
+            <img src="../img/account-icon-user-icon-vector-graphics_292645-552.avif" alt="Nombre del usuario">
+            <h3><?php echo $_SESSION['user_name']; ?> <p>Usuario</p>
+            </h3>
+
+        </div>
+        <button class="about-button">¿Qué es esta aplicación?</button>
+
+
+
     </header>
     <section>
         <aside class="menu">
-            <!-- Contenido del menú desplegable -->
             <ul>
-                <li><a href="#">Opción 1</a></li>
-                <li><a href="#">Opción 2</a></li>
-                <li><a href="#">Opción 3</a></li>
+                <div class="head-menu">
+                    <li><a href="#">Página Principal</a></li>
+                    <li><a href="Calendario/Calendario.php">Calendario</a></li>
+                    </div>
+                    <li><a href="#">Tareas Activas</a></li>
+                    <li><a href="#">Tareas Terminadas</a></li>
+                    <li><a href="../InicioSesion/logout.php">Cerrar Sesión</a></li>
             </ul>
         </aside>
         <main>
@@ -74,25 +74,25 @@ if (!isset($_SESSION['user_name'])) {
                     $result = $conexion->exeqSelect($sql);
 
                     if ($result) {
-                      while ($row = mysqli_fetch_assoc($result)) {
-                          echo "<div class='project-box'>";
-                          echo "<div class='project-header'>";
-                          echo "<div class='project-number'>" . $row["numProyecto"] . "</div>";
-                          echo "<div class='project-name'>" . $row["Nombre"] . "</div>";
-                          echo "<div class='project-location'>" . $row['Ubicacion'] . "</div>";
-                          echo "</div>";
-                          echo "<div class='project-dates'>";
-                          echo "<div class='project-date'>" . $row['fechaInicio'] . "</div>";
-                          echo "<div class='project-state'>" . $row['Estado'] . "</div>";
-                          echo "<div class='project-date'>" . $row['fechaFinal'] . "</div>";
-                          echo "</div>";
-                          echo "<a href='Proyectos.php?data-numProyecto=" . $row["numProyecto"] . "' class='details-button'>Ver detalles</a>";
-                          echo "</div>";
-                      }
-                      echo "</div>";
-                  } else {
-                      echo "No se encontraron proyectos.";
-                  }
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<div class='project-box'>";
+                            echo "<div class='project-header'>";
+                            echo "<div class='project-number'>" . $row["numProyecto"] . "</div>";
+                            echo "<div class='project-name'>" . $row["Nombre"] . "</div>";
+                            echo "<div class='project-location'>" . $row['Ubicacion'] . "</div>";
+                            echo "</div>";
+                            echo "<div class='project-dates'>";
+                            echo "<div class='project-date'>" . $row['fechaInicio'] . "</div>";
+                            echo "<div class='project-state'>" . $row['Estado'] . "</div>";
+                            echo "<div class='project-date'>" . $row['fechaFinal'] . "</div>";
+                            echo "</div>";
+                            echo "<a href='Proyectos.php?data-numProyecto=" . $row["numProyecto"] . "' class='details-button'>Ver detalles</a>";
+                            echo "</div>";
+                        }
+                        echo "</div>";
+                    } else {
+                        echo "No se encontraron proyectos.";
+                    }
                 }
                 ?>
             </div>
@@ -100,4 +100,5 @@ if (!isset($_SESSION['user_name'])) {
     </section>
     <div class="hero"></div>
 </body>
+
 </html>
