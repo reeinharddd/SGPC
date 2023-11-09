208f25af9597d0b7c5ff4f535b356beec8bfe52c
Erik <beltraberik@gmail.com>
2023-11-09 10:20:42 -0800

    Se agrego la generacion de contrasena aleatoria

--- MenuAdmin/RegistroUsuarios/addUser.php
+++ MenuAdmin/RegistroUsuarios/addUser.php
@@ -8,8 +8,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $apMat = $_POST['apMat'];
     $numero = $_POST['numero'];
     $email = $_POST['email'];
-    $pass = $_POST['password'];
-    $cpass = $_POST['cpassword'];
+    $comb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); 
+    $combLen = strlen($comb) - 1; 
+    for ($i = 0; $i < 8; $i++) {
+        $n = rand(0, $combLen);
+        $pass[] = $comb[$n];
+    }
+    $pass = (implode($pass));
     $user_type = $_POST['user_type'];
 
     $select = "SELECT * FROM Usuario WHERE email = '$email'";
@@ -18,10 +24,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (mysqli_num_rows($result) > 0) {
         echo 'Usuario existe';
     } else {
-        if ($pass != $cpass) {
-            echo 'Contras no cuadran';
-            header('location:register_form.php');
-        } else {
+            session_start();
+            $_SESSION['user_data'] = array(
+                'Nombre' => $name,
+                'Apellido Paterno' => $apPat,
+                'Apellido Materno' => $apMat,
+                'Número de Teléfono' => $numero,
+                'Email' => $email,
+                'Contraseña' => $pass,
+                'Tipo de Usuario' => $user_type,
+              
+            );
             $user->setNombre($name);
             $user->setApPat($apPat);
             $user->setApMat($apMat);
@@ -33,15 +46,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $newUserId = $user->setNewUser();
             if ($newUserId > 0) {
                 echo 'Usuario registrado';
-                header('location:../index.php');
+                header('location:confirmacionRegistro.php');
             } else {
                 echo 'Algo salio mal';
             }
-        }
+        
     }
 }
-
-
-
-
-?>
