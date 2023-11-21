b53b81280597028e9db850418d1fc1d49ee7806a
Ramon Marquez Alvarado <ramonmrqzal@gmail.com>
2023-11-21 11:25:42 -0800

    Calendario

---
+++ MenuAdmin/Calendario/Calendario.php
@@ -0,0 +1,58 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if (!isset($_SESSION['admin_name'])) {
+   header('location:../../Alertas/warning.html');
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario Interactivo</title>
+    <link rel="stylesheet" href="../../css/calendario.css">
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="icon" href="../../img/Logo1.png" type = "image/png">
+</head>
+<body>
+    <header class = "header">
+        <div><img src="../../img/Logo1.png" alt="" width="100px" height="100px"></div>
+        <input type="checkbox" id = "toggle">
+        <label for="toggle"> <img src="../../img/menu.svg" alt="menu"></label>
+        <nav class = "navigation">
+          <ul>
+            <li><a href="../index.php">Inicio</a></li>
+            <li><a href="#">Proyectos</a>
+                <ul>
+                    <li><a href="../Proyectos.php">Proyectos vigentes</a></li>
+                    <li><a href="../AllProyectos.php">Proyectos completados</a></li>
+                </ul>
+              </li>
+              <li><a href="../Contactos.php">Contacto</a></li>
+          </ul>
+        </nav>
+      </header>
+
+    <div id="calendar-container">
+        <button id="prev-month">←</button>
+        <h2 id="month-year"></h2>
+        <button button id="next-month">→</button>
+        <div id="calendar"></div>
+    </div>
+
+    <div id="popup" class="popup">
+        <div class="popup-content">
+            <span class="close-btn">&times;</span>
+            <h3>Proyectos del dia</h3>
+            <p id="fecha-info">Información de la fecha seleccionada aquí</p>
+        </div>
+    </div>
+
+    
+    <div><script src="script.js"></script></div>
+</body>
+</html>
--- MenuUsuario/Calendario/detalles_fecha.php
+++ MenuUsuario/Calendario/detalles_fecha.php
@@ -10,23 +10,60 @@ if (!isset($_SESSION['user_name'])) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/calendario.css">
+    <link rel="stylesheet" href="../../css/main.css">
     <title>Detalles de la Fecha</title>
-</head>
-<body>
-    <a href="../../index.php">Regresar al menú</a>
-    <h1>Detalles de la Fecha</h1>
-    <p id="fechaSeleccionada">Cargando fecha...</p>
+    </head>
+    <body>
+        <main>
+            <div class="project-list">
+                <?php
+                include "../../conexion.php";
 
-    <script>
-        const urlParams = new URLSearchParams(window.location.search);
-        const day = urlParams.get('day');
-        const month = urlParams.get('month');
-        const year = urlParams.get('year');
+                $fechaSeleccionada = $_POST['fecha'];
+                $conexion = new conexion();
+                if ($conexion->connect()) {
+                    $con = $conexion->getConexion();
+                    $sql = "SELECT P.idProyecto, P.nombre, P.descripcion, P.ubicacion, P.fechaInicio, P.fechaFinal, P.estado 
+                        FROM Proyecto P 
+                        INNER JOIN UsuarioProyecto UP ON P.idProyecto = UP.idProyecto
+                        WHERE UP.idUsuario = ".$_SESSION['id']." AND P.fechaInicio <= '".$fechaSeleccionada."' AND P.fechaFinal >= '".$fechaSeleccionada."'";
 
-        document.getElementById('fechaSeleccionada').textContent = `Has seleccionado: ${day}/${month}/${year}`;
-    </script>
-</body>
+                    $result = $conexion->exeqSelect($sql);
+                    if ($result) {
+
+                        while ($row = mysqli_fetch_assoc($result)) {
+
+                            echo "<div class='project-box'>";
+
+                            echo "<div class='project-header'>";
+                            echo "<div class='project-number'>" . $row["idProyecto"] . "</div>";
+                            echo "<div class='project-name'>" . $row["nombre"] . "</div>";
+                            echo "<div class='project-location'>" . $row['ubicacion'] . "</div>";
+                            echo "</div>";
+
+                            echo "<div class='project-dates'>";
+                            echo "<div class='project-date'>" . $row['fechaInicio'] . "</div>";
+                            echo "<div class='project-state'>" . $row['estado'] . "</div>";
+                            echo "<div class='project-date'>" . $row['fechaFinal'] . "</div>";
+                            echo "</div>";
+
+                            echo "<a href='../Proyectos.php?idProyecto=" . $row["idProyecto"] . "' class='details-button'>Ver detalles</a>";
+
+                            echo "</div>";
+                        }
+
+                        echo "</div>";
+                    } else {
+
+                        echo "No se encontraron proyectos.";
+                    }
+                }
+                ?>
+            </div>
+        </main>
+    </body>
 </html>
