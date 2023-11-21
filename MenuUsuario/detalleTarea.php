16087b8f839b9147a562e887f1d49f74992aa9be
Erik <beltraberik@gmail.com>
2023-11-20 22:33:36 -0800

    Se agregaron funciones para hacer comentarips, y marcar tarea como finalizada, comentarios y mostrar datos de la tarea (completado) estado de la tarea, pendiente

--- MenuUsuario/detalleTarea.php
+++ MenuUsuario/detalleTarea.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 session_start();
 
 if (!isset($_SESSION['user_name'])) {
@@ -14,12 +15,13 @@ $proyectos = $consultas->getProyectos();
 
 if ($proyectos) {
     $proyecto = isset($_GET['idProyecto']) ? $_GET['idProyecto'] : null;
-    $idProyecto = $_SESSION['idProyecto'];
+    $idProyecto = isset($_GET['idProyecto']) ? $_GET['idProyecto'] : null;
 
     if ($proyecto !== null) {
         $tareas = $consultas->getTareas($proyecto);
         $idTarea = isset($_GET['idTarea']) ? $_GET['idTarea'] : null;
         $tareaSeleccionada = null;
+
 
         foreach ($tareas as $tarea) {
             if ($tarea['idTarea'] == $idTarea) {
@@ -29,22 +31,31 @@ if ($proyectos) {
         }
 
         if ($tareaSeleccionada) {
+            $infoProyecto = $consultas->getInfoProyecto($proyecto);
+
             $usuarioAsignado = $consultas->getUsuarioAsignado($tareaSeleccionada['idTarea']);
             $comentarios = $consultas->getComentariosTarea($tareaSeleccionada['idTarea']);
 
-           
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SGPC</title>
+    <link rel="stylesheet" href="../css/detalleTarea.css">
+    <link rel="icon" href="../img/Logo1.png" type="image/png">
 </head>
 
 <body>
     <?php
+
                 include "plantillas/header.php";
-                include "plantillas/miniBar.php";
                 include "plantillas/menu.php";
+                ?>
+    <?php
 
                 if ($tareaSeleccionada) {
                     $infoProyecto = $consultas->getInfoProyecto($tareaSeleccionada['idProyecto']);
@@ -52,14 +63,20 @@ if ($proyectos) {
                     $comentarios = $consultas->getComentariosTarea($tareaSeleccionada['idTarea']);
                 ?>
     <main>
+        <form action="marcar_completa.php" method="post">
+            <input type="hidden" name="idTarea" value="<?= $tareaSeleccionada['idTarea']; ?>">
+            <input type="submit" value="Marcar como Completa">
+        </form>
         <div class="task-details">
             <h2><?= $tareaSeleccionada['NombreTarea']; ?></h2>
-            <p><strong>Proyecto:</strong> <?= $infoProyecto['nombre']; ?></p>
-            <p><strong>Fecha de Inicio:</strong> <?= $tareaSeleccionada['fechaInicio']; ?></p>
-            <p><strong>Fecha Final:</strong> <?= $tareaSeleccionada['fechaFinal']; ?></p>
+            <p><strong>Fecha de Inicio:</strong> <?= date("Y-m-d", strtotime($tareaSeleccionada['fechaInicio'])); ?></p>
+            <p><strong>Fecha Final:</strong>
+                <?= ($tareaSeleccionada['fechaFinal'] !== null) ? date("Y-m-d", strtotime($tareaSeleccionada['fechaFinal'])) : 'Sin fecha final'; ?>
+            </p>
+
+
             <p><strong>Estado:</strong> <?= $consultas->obtenerNombreEstado($tareaSeleccionada['estado']); ?></p>
             <p><strong>Descripción:</strong> <?= $tareaSeleccionada['DescripcionTarea']; ?></p>
-
 
             <?php if (!empty($comentarios)) : ?>
             <div class="comments-section">
@@ -72,9 +89,23 @@ if ($proyectos) {
                 </div>
                 <?php endforeach; ?>
             </div>
+            <?php else : ?>
+            <p>No hay comentarios disponibles.</p>
             <?php endif; ?>
+            <div class="add-comment-section">
+                <h3>Agregar Comentario</h3>
+                <form action="agregar_comentario.php" method="post">
+                    <textarea name="comentario" placeholder="Escribe tu comentario aquí..." required></textarea>
+                    <input type="hidden" name="idTarea" value="<?= $tareaSeleccionada['idTarea']; ?>">
+                    <input type="submit" value="Enviar Comentario">
+                </form>
+            </div>
         </div>
     </main>
+
+
+
+
     <?php
                 } else {
                     echo "No se encontró la tarea seleccionada.";
